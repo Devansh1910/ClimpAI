0f682517cf0bb6c03188a6dc03dfb8aef0e1d47e
DenoiserAi <devanshsaxena1019@gmail.com>
2024-07-11 22:52:55 +0530

    Initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      NEXT_PUBLIC_CLERK_FRONTEND_API: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API,
+    },
+  };
+  
+  export default nextConfig;
+  
