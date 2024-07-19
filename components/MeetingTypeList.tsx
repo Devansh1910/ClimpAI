d59d8125adb57ebddd83a7b6fbd7aeb011d48230
DenoiserAi <devanshsaxena1019@gmail.com>
2024-07-20 00:40:38 +0530

    Update the upcoming in the Home

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -105,10 +105,8 @@ const MeetingTypeList = () => {
         handleClick={() => setMeetingState("isJoiningMeeting")}
         className="bg-yellow-500"
       />
-      <section className="flex size-full flex-col gap-10 text-white">
-        <h1 className="text-3xl font-bold">Upcoming</h1>
+
         <CallList type="upcoming" />
-      </section>
 
       {!callDetails ? (
         <MeetingModel
